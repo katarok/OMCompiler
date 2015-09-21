6871a31910f7e03a6341c1207e7001e8d2a8a915
Michael Kloeppel <michael.kloeppel@tu-dresden.de>
2015-09-21 17:25:52 +0200

    first implementation for CppDASSL - uses OpenBLAS - preliminary work for parallel Jacobian

---
+++ SimulationRuntime/cpp/Include/Solver/CppDASSL/CppDASSL.h
@@ -0,0 +1,198 @@
+
+#pragma once
+
+#include "FactoryExport.h"
+
+#include <Core/Solver/SolverDefaultImplementation.h>
+#include <Core/Utils/extension/measure_time.hpp>
+
+int ddaskr_ (
+    int (*RES)(const double* t,const double* y,const double* yprime, double* cj, double* delta, int* ires, void *),
+    int* NEQ,
+    double* T,
+    double* Y,
+    double* YPRIME,
+    double* TOUT,
+    int* INFO,
+    double* RTOL,
+    double* ATOL,
+    int* IDID,
+    double* RWORK,
+    int* LRW,
+    int* IWORK,
+    int* LIW,
+    void* PAR,
+    int (*JAC)(double *, int *, int *, double *, double *, double *, double *, double *, double *, double *, double *, double *, int *, int *, void *),
+    int (*PSOL)(int *NEQ, double* T, double* Y, double* YPRIME, double* SAVR, double* WK, double* CJ, double* WGHT, double * WP, int* IWP, double* B, double* EPLIN, int* IER, void *),
+    int (*RT)(int* NEQ,double* T,double* Y,double* YP,int* NRT, double* RVAL, void *),
+    int* NRT,
+    int* JROOT
+);
+
+/*****************************************************************************/
+// Peer
+// BDF-Verfahren für steife und nicht-steife ODEs
+// Dokumentation siehe offizielle Peer Doku
+
+/*****************************************************************************
+Copyright (c) 2014, IWR TU Dresden, All rights reserved
+*****************************************************************************/
+
+class CppDASSL
+  : public ISolver,  public SolverDefaultImplementation
+{
+public:
+
+  CppDASSL(IMixedSystem* system, ISolverSettings* settings);
+
+  virtual ~CppDASSL();
+
+  // geerbt von Object (in SolverDefaultImplementation)
+  //---------------------------------------
+  /// Spezielle Solvereinstellungen setzten (default oder user defined)
+  virtual void initialize();
+
+
+  // geerbt von ISolver
+  //---------------------------------------
+  /// Setzen der Startzeit für die numerische Lösung
+  virtual void setStartTime(const double& time)
+  {
+    SolverDefaultImplementation::setStartTime(time);
+  };
+
+  /// Setzen der Endzeit für die numerische Lösung
+  virtual void setEndTime(const double& time)
+  {
+    SolverDefaultImplementation::setEndTime(time);
+  };
+
+  /// Setzen der initialen Schrittweite (z.B. auch nach Nullstelle)
+  virtual void setInitStepSize(const double& stepSize)
+  {
+    SolverDefaultImplementation::setInitStepSize(stepSize);
+  };
+
+  /// Berechung der numerischen Lösung innerhalb eines gegebenen Zeitintervalls
+  virtual void solve(const SOLVERCALL command = UNDEF_CALL);
+
+  /// Liefert den Status des Solvers nach Beendigung der Simulation
+  virtual ISolver::SOLVERSTATUS getSolverStatus()
+  {
+    return (SolverDefaultImplementation::getSolverStatus());
+  };
+
+  //// Ausgabe von statistischen Informationen (wird vom SimManager nach Abschluß der Simulation aufgerufen)
+  virtual void writeSimulationInfo();
+
+
+  virtual int reportErrorMessage(std::ostream& messageStream);
+  virtual bool stateSelection();
+  virtual void setTimeOut(unsigned int time_out);
+    virtual void stop();
+private:
+
+
+
+
+
+  // Nulltellenfunktion
+  void writeCppDASSLOutput(const double &time,const double &h,const int &stp);
+
+  ISolverSettings
+    *_cppdasslsettings;              ///< Input      - Solver settings
+
+  int
+    _nrt,
+    _idid,
+    *_info,
+    _lrw,
+    _liw,
+    *_iwork;
+  double
+    _atol,
+    _rtol,
+    _h,
+    *_rwork,
+    *_y,
+    *_yp;
+  long int
+    _dimSys;                 ///< Input       - (total) Dimension of system (=number of ODE)
+
+  void
+    *_data;
+
+
+
+  // Variables for Coloured Jacobians
+//  int  _sizeof_sparsePattern_colorCols;
+//  int* _sparsePattern_colorCols;
+//
+//  int  _sizeof_sparsePattern_leadindex;
+//  int* _sparsePattern_leadindex;
+//
+//
+//  int  _sizeof_sparsePattern_index;
+//  int* _sparsePattern_index;
+//
+//
+//  int  _sparsePattern_maxColors;
+//
+//  bool _cvode_initialized;
+
+
+//   ISystemProperties* _properties;
+   IContinuous* _continuous_system[5];
+//   IEvent* _event_system;
+//   IMixedSystem* _mixed_system;
+   ITime* _time_system[5];
+
+//   std::vector<MeasureTimeData> measureTimeFunctionsArray;
+//   MeasureTimeValues *measuredFunctionStartValues, *measuredFunctionEndValues;
+
+};
+#else
+class CppDASSL : public ISolver, public SolverDefaultImplementation
+{
+public:
+	CppDASSL(IMixedSystem* system, ISolverSettings* settings) : ISolver(), SolverDefaultImplementation(system, settings)
+	{
+		throw std::runtime_error("Peer solver is not available.");
+	}
+
+	virtual void setStartTime(const double& time)
+	{}
+
+	virtual void setEndTime(const double& time)
+	{}
+
+	virtual void setInitStepSize(const double& stepSize)
+	{}
+
+	virtual void initialize()
+	{}
+
+	virtual bool stateSelection()
+	{
+		throw std::runtime_error("Peer solver is not available.");
+	}
+
+	virtual void solve(const SOLVERCALL command = UNDEF_CALL)
+	{}
+
+	virtual SOLVERSTATUS getSolverStatus()
+	{ return UNDEF_STATUS; }
+
+	virtual void setTimeOut(unsigned int time_out)
+	{}
+
+	virtual void stop()
+	{}
+
+	virtual void writeSimulationInfo()
+	{}
+private:
+    static int res(const double* t, const double* y, const double* yprime, double* cj, double* delta, int* ires, void *par);
+    int calcFunction(const double* t, const double* y, const double* yprime, double* cj, double* delta, int* ires);
+};
+#endif
---
+++ SimulationRuntime/cpp/Solver/CppDASSL/CppDASSL.cpp
@@ -0,0 +1,234 @@
+#include <Core/ModelicaDefine.h>
+#include <Core/Modelica.h>
+
+#include <Core/Math/Functions.h>
+//#include <Core/Math/ILapack.h>
+#include <Solver/CppDASSL/CppDASSL.h>
+
+
+
+#include "omp.h"
+
+
+CppDASSL::CppDASSL(IMixedSystem* system, ISolverSettings* settings)
+    : SolverDefaultImplementation(system, settings),
+      _peersettings(dynamic_cast<ISolverSettings*>(_settings)),
+      _h(2e-2),
+      _continuous_system(),
+      _time_system()
+/*      _cvodeMem(NULL),
+      _z(NULL),
+      _zInit(NULL),
+      _zWrite(NULL),
+      _dimSys(0),
+      _cv_rt(0),
+      _outStps(0),
+      _locStps(0),
+      _idid(0),
+      _hOut(0.0),
+      _tOut(0.0),
+      _tZero(0.0),
+      _zeroSign(NULL),
+      _absTol(NULL),
+      _cvode_initialized(false),
+      _tLastEvent(0.0),
+      _event_n(0),
+      _properties(NULL),
+      _continuous_system(NULL),
+      _event_system(NULL),
+      _mixed_system(NULL),
+      _time_system(NULL),
+    _delta(NULL),
+    _ysave(NULL) */
+{
+  _data = ((void*) this);
+}
+
+CppDASSL::~CppDASSL()
+{
+  if (_y)
+    delete [] _y;
+  if (_yp)
+    delete [] _yp;
+  if (_info)
+    delete [] _info;
+  if (_rwork)
+    delete [] _rwork;
+  if (_iwork)
+    delete [] _iwork;
+}
+
+void CppDASSL::initialize()
+{
+    IContinuous *continuous_system = dynamic_cast<IContinuous*>(_system);
+    ITime *time_system =  dynamic_cast<ITime*>(_system);
+    _time_system[0] = time_system;
+    _continuous_system[0] = continuous_system;
+/*
+    for(int i = 1; i < 5; i++)
+    {
+        IMixedSystem* clonedSystem = _system->clone();
+        _continuous_system[i] = dynamic_cast<IContinuous*>(clonedSystem);
+        _time_system[i] = dynamic_cast<ITime*>(clonedSystem);
+        dynamic_cast<ISystemInitialization*>(clonedSystem)->initialize();
+    }*/
+
+    SolverDefaultImplementation::initialize();
+    _dimSys = _continuous_system[0]->getDimContinuousStates();
+    _h = std::max(std::min(_h, _cppdasslsettings->getUpperLimit()), _cppdasslsettings->getLowerLimit());
+    _y = new double[_dimSys];
+    _yp = new double[_dimSys];
+    if(_info)
+        delete [] _info;
+    if(_rwork)
+        delete [] _rwork;
+    if(_iwork)
+        delete [] _iwork;
+    _info=new int[20];
+    _rtol=1e-6;
+    _atol=1e-6;
+    _rwork=new double[60+9*size+size*size];
+    _lrw=60+9*size+size*size;
+    _iwork=new int[40+size];
+    _liw=40+size;
+    _info[0]=0;
+    _info[1]=0;
+    _info[2]=0;
+    _info[3]=0;
+    _info[4]=0;
+    _info[5]=0;
+    _info[6]=0;
+    _info[7]=0;
+    _info[8]=0;
+    _info[9]=0;
+    _info[10]=0;
+    _info[11]=0;
+    _info[12]=0;
+    _info[13]=0;
+    _info[14]=0;
+    _info[15]=0;
+    _info[16]=0;
+    _info[17]=0;
+    _nrt=0;
+    _continuous_system[0]->evaluateAll(IContinuous::ALL);
+    _continuous_system[0]->getContinuousStates(_y);
+}
+
+
+
+void CppDASSL::solve(const SOLVERCALL action)
+{
+    double t;
+    if ((action & RECORDCALL) && (action & FIRST_CALL)) {
+        initialize();
+        return;
+    }
+
+  // Initialization phase
+    t=_tCurrent;
+    _timeSystem[0]->setTime(t);
+    _continuousSystem[0]->setContinuousStates(_y);
+    _continuousSystem[0]->evaluateODE(IContinuous::ALL);    // vxworksupdate
+    _continuousSystem[0]->getRHS(_yp);
+    SolverDefaultImplementation::writeToFile(0, t, _h);
+
+    ddaskr_(&res,&_dimSys,&t,&_y[0],&_yp[0],&_tEnd,_info,&_rtol,&_atol,&_idid,_rwork,&_lrw,_iwork,&_liw,NULL,NULL,NULL,NULL,&_nrt,NULL);
+    while(_idid==-1) {
+        _info[0]=1;
+        ddaskr_(&res,&_dimSys,&t,&_y[0],&_yp[0],&_tEnd,_info,&_rtol,&_atol,&_idid,_rwork,&_lrw,_iwork,&_liw,NULL,NULL,NULL,NULL,&_nrt,NULL);
+    }
+    _tCurrent=_tEnd;
+    _time_system[0]->setTime(_tCurrent);
+    _continuous_system[0]->setContinuousStates(_y);
+    _continuous_system[0]->evaluateAll(IContinuous::ALL);
+    SolverDefaultImplementation::writeToFile(0, t, _h);
+    _solverStatus = ISolver::DONE;
+}
+
+
+
+void CppDASSL::writeCppDASSLOutput(const double &time, const double &h, const int &stp)
+{
+  /*
+  if (stp > 0)
+  {
+    if (_cvodesettings->getDenseOutput())
+    {
+      _bWritten = false;
+      double *oldValues = NULL;
+
+      //We have to find all output-points within the last solver step
+      while (_tLastWrite + dynamic_cast<ISolverSettings*>(_cvodesettings)->getGlobalSettings()->gethOutput() <= time)
+      {
+        if (!_bWritten)
+        {
+          //Rescue the calculated derivatives
+          oldValues = new double[_continuous_system->getDimRHS()];
+          _continuous_system->getRHS(oldValues);
+        }
+        _bWritten = true;
+        _tLastWrite = _tLastWrite + dynamic_cast<ISolverSettings*>(_cvodesettings)->getGlobalSettings()->gethOutput();
+        //Get the state vars at the output-point (interpolated)
+        _idid = CVodeGetDky(_cvodeMem, _tLastWrite, 0, _CV_yWrite);
+        _time_system->setTime(_tLastWrite);
+        _continuous_system->setContinuousStates(NV_DATA_S(_CV_yWrite));
+        _continuous_system->evaluateAll(IContinuous::CONTINUOUS);
+        SolverDefaultImplementation::writeToFile(stp, _tLastWrite, h);
+      }      //end if time -_tLastWritten
+      if (_bWritten)
+      {
+        _time_system->setTime(time);
+        _continuous_system->setContinuousStates(_z);
+        _continuous_system->setRHS(oldValues);
+        delete[] oldValues;
+        //_continuous_system->evaluateAll(IContinuous::CONTINUOUS);
+      }
+      else if (time == _tEnd && _tLastWrite != time)
+      {
+        _idid = CVodeGetDky(_cvodeMem, time, 0, _CV_y);
+        _time_system->setTime(time);
+        _continuous_system->setContinuousStates(NV_DATA_S(_CV_y));
+        _continuous_system->evaluateAll(IContinuous::CONTINUOUS);
+        SolverDefaultImplementation::writeToFile(stp, _tEnd, h);
+      }
+    }
+    else
+      SolverDefaultImplementation::writeToFile(stp, time, h);
+  }
+  */
+}
+
+int CppDASSL::res(const double* t, const double* y, const double* yprime, double* cj, double* delta, int* ires, void *par) {
+    return ((CppDASSL*) par)->calcFunction(t, y, yprime, cj, delta, ires);
+}
+
+int CppDASSL::calcFunction(const double* t, const double* y, const double* yprime, double* cj, double* delta, int* ires) {
+    _timeSystem[0]->setTime(t);
+    _continuousSystem[0]->setContinuousStates(y);
+    _continuousSystem[0]->evaluateODE(IContinuous::ALL);    // vxworksupdate
+    _continuousSystem[0]->getRHS(delta);
+    for(int i=0; i<_dimSys; ++i) delta[i]=yprime[i]-delta[i];
+    return 0;
+}
+
+bool CppDASSL::stateSelection()
+{
+  return SolverDefaultImplementation::stateSelection();
+}
+
+void CppDASSL::setTimeOut(unsigned int time_out)
+  {
+       SimulationMonitor::setTimeOut(time_out);
+  }
+ void CppDASSL::stop()
+  {
+       SimulationMonitor::stop();
+  }
+
+
+void CppDASSL::setcycletime(double cycletime){}
+void CppDASSL::writeSimulationInfo(){}
+int CppDASSL::reportErrorMessage(std::ostream& messageStream) {
+    return 0;
+}
+#endif
